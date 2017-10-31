7fd4b46b225510ccce1610f41d0668297067d1d9
P-Eldritch <juodojitulzis@gmail.com>
2017-10-31 18:00:20 +0200

    final touches

--- indexiukas.js
+++ indexiukas.js
@@ -1,17 +1,10 @@
-// alert("Hello from JS!");
-
-
-// var clickas = document.querySelectorAll("header")[0];
-// console.log(clickas);
-// var a = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-// window.addEventListener("scroll", function(){
-// 	    clickas.classList.add("vazinetojas");
-// 	    console.log(a);
-// });
 
 document.addEventListener('DOMContentLoaded', function() {
+
+// prilimpa nav juosta pascrollinus ir dingsta grizus i 0 pozicija
+
 	var clickas = document.querySelectorAll("header")[0];
-window.addEventListener("scroll", function(){
+	window.addEventListener("scroll", function(){
 	 	var y = window.scrollY;
 	    if (y>0) {
 	    	clickas.classList.add("vazinetojas");
@@ -22,190 +15,85 @@ window.addEventListener("scroll", function(){
 	    }
 	});
 
-var box = document.querySelector('.carouselbox');
-  var first = box.querySelector('.first');
-  var firstContent = box.querySelector('.reviews-1');
-  var second = box.querySelector('.second');
-  var secondContent = box.querySelector('.reviews-2');
-  var third = box.querySelector('.third');
-  var thirdContent = box.querySelector('.reviews-3');
-
-// su mod pabandyt:
-secondContent.classList.add("hidden-class");
-thirdContent.classList.add("hidden-class");
-
-first.addEventListener("click", function(){
-secondContent.classList.add("hidden-class");
-firstContent.classList.remove("hidden-class");
-thirdContent.classList.add("hidden-class");
-
-});
-
-second.addEventListener("click", function(){
-secondContent.classList.remove("hidden-class");
-firstContent.classList.add("hidden-class");
-thirdContent.classList.add("hidden-class");
-
-});
-
-third.addEventListener("click", function(){
-secondContent.classList.add("hidden-class");
-firstContent.classList.add("hidden-class");
-thirdContent.classList.remove("hidden-class");
-
-});
-
-// PopUp langas paspaudus
-// var KarbisPhoto = document.querySelectorAll(".member-photo");
-// var KarbisText = document.querySelectorAll(".pop");
-// for(var i=0; i < KarbisPhoto.length; i++) {
-// 	KarbisPhoto[i].addEventListener("click", function(num){
-// 	    return function() {
-//             KarbisText[num].classList.add("hovered-photo");
-//         };
-//     }(i)); 
-// }
-
-
-
-// (function(){
-//     var clicked = false;
-//     document.querySelector('.member-photo').addEventListener('click', function(){
-//         clicked = clicked ? false : true;
-//     });
-//     document.querySelector('.pop').addEventListener('click', function(){
-//         if (clicked) {
-//             this.classList.toggle("hovered-photo"); //bus problemu su THIS
-//         }
-//         clicked = false;
-//     });
-// })();
-
-
-var KarbisPhoto = document.querySelectorAll(".member-photo");
-var KarbisText = document.querySelectorAll(".pop");
-
+//issoka teksto langas paspaudus ant fotkes
+	var KarbisPhoto = document.querySelectorAll(".member-photo");
+	var KarbisText = document.querySelectorAll(".pop");
+	var header = document.querySelector("header");
 	var clicked = false;
-	for(var i=0; i < KarbisPhoto.length; i++) {
-		KarbisPhoto[i].addEventListener("click", function(num){
-			console.log(clicked);
-		clicked = clicked ? false : true;	
-	    return function(clicked) {
-            KarbisText[num].classList.add("hovered-photo");
-            //for gal id4t, kad visas sekcijas nudazytu
 
-            document.querySelector("header").classList.add("nerodyti-header");
-            document.querySelector("header").classList.remove("vazinetojas");
-            if (clicked) {
-            	console.log(num);
-             	KarbisText[num].addEventListener('click', function(){
-             		console.log(num);
-           			KarbisText[num].classList.remove("hovered-photo"); 
-           			document.querySelector("header").classList.remove("nerodyti-header");
-            		document.querySelector("header").classList.add("vazinetojas"); 
-            		clicked = false;
+		for(var i=0; i < KarbisPhoto.length; i++) {
+			KarbisPhoto[i].addEventListener("click", function(num){
+			clicked = clicked ? false : true;	
+		    return function(clicked) {
+	            KarbisText[num].classList.add("hovered-photo");
+	            //paspaudus paslepia header nav juosta
+	            header.classList.add("nerodyti-header");
+	            header.classList.remove("vazinetojas");
+	            if (clicked) {
+	            	//paspaudus lauk kliko ant fotkes ir isjungs tada
+	             	KarbisText[num].addEventListener('click', function(){
+	           			KarbisText[num].classList.remove("hovered-photo"); 
+	           			header.classList.remove("nerodyti-header");
+	            		header.classList.add("vazinetojas"); 
+	            		clicked = false;
+	        		});
+	    	}
+	    };
+	    }(i)); //i num parametra perduoda i is ciklo
+		};
 
-            		console.log(clicked);
-            		// return clicked;
-        		});
-    	}
-    };
-    }(i)); 
-	};
+//burgeris
 
+	document.querySelector(".main-nav").addEventListener("click", function(){
+		this.classList.toggle("open");
+	});
 
+//reviewsu perziura
+	var items = document.querySelectorAll(".reviews");
+	var prev = document.querySelector('.prev');
+	var next= document.querySelector('.next');
+	var sitas  = items[0];
+	var ilgis = items.length;
+	var counter = 0;
+	var trukme = 6000;
+	sitas.classList.add("visible"); //visi reviewsai nematomi, uzdedamas matomumas paciam pirmam su klase 'visible'
 
+	setInterval(function() {move(1)}, trukme);
 
- // var next = box.querySelector('.next');
- //  var prev = box.querySelector('.prev');
- //  var items = box.querySelectorAll('.content li');
- //  var counter = 0;
- //  var amount = items.length;
- //  var current = items[0];
- //  box.classList.add('active');
- //  function navigate(direction) {
- //    current.classList.remove('current');
- //    counter = counter + direction;
- //    if (direction === -1 && 
- //        counter < 0) { 
- //      counter = amount - 1; 
- //    }
- //    if (direction === 1 && 
- //        !items[counter]) { 
- //      counter = 0;
- //    }
- //    current = items[counter];
- //    current.classList.add('current');
- //  }
- //  next.addEventListener('click', function(ev) {
- //    navigate(1);
- //  });
- //  prev.addEventListener('click', function(ev) {
- //    navigate(-1);
- //  });
- //  navigate(0);
-//  var tekstai = document.querySelectorAll(".treatments-section p")[0];
-// var butonai = document.querySelectorAll(".treatments-section h3")[0];
-// butonai.addEventListener("click", function(){
-// 	tekstai.classList.toggle("clicked-treatment");
+	function move(step) {
+		if (step === 1 && counter<items.length-1) {
+			sitas.classList.remove("visible");
+			counter= counter+1;
+			sitas = items[counter];
+			sitas.classList.add("visible");
+		}
+		//persuka is naujo ejima, kai pasiekia paskutini elementa
+		else if (step === 1 && counter>=items.length-1) {
+			sitas.classList.remove("visible");
+			counter =0;
+			sitas = items[counter];
+			sitas.classList.add("visible");
+		}
+		if (step === -1 && counter >0 ) {
+			sitas.classList.remove("visible");
+			counter= counter-1;
+			sitas = items[counter];
+			sitas.classList.add("visible");
+		}
+		//permeta i paskutini reviewsa, kai pasiekia pirma elementa
+			else if (step === -1 && counter===0) {
+			sitas.classList.remove("visible");
+			counter =items.length-1;
+			sitas = items[counter];
+			sitas.classList.add("visible");
+		}
 
-// 	console.log("au");
-// });
-
-//prideti kita klase su kursuriomi i butoais
+	}
+	prev.addEventListener("click", function(){
+		move(-1);
+	});
+	next.addEventListener("click", function(){
+		move(1);
+	});
 
 });
-
-
-
-//   var box = document.querySelector('.carouselbox');
-//   var first = box.querySelector('.first');
-//   var firstContent = box.querySelector('.reviews-1');
-//   var second = box.querySelector('.second');
-//   var secondContent = box.querySelector('.reviews-2');
-//   var third = box.querySelector('.third');
-//   var thirdContent = box.querySelector('.reviews-3');
-
-
-// secondContent.classList.add("hidden-class");
-// thirdContent.classList.add("hidden-class");
-
-// first.addEventListener("click", function(){
-// secondContent.classList.add("hidden-class");
-// firstContent.classList.remove("hidden-class");
-// thirdContent.classList.add("hidden-class");
-
-// });
-
-// second.addEventListener("click", function(){
-// secondContent.classList.remove("hidden-class");
-// firstContent.classList.add("hidden-class");
-// thirdContent.classList.add("hidden-class");
-
-// });
-
-// third.addEventListener("click", function(){
-// secondContent.classList.add("hidden-class");
-// firstContent.classList.add("hidden-class");
-// thirdContent.classList.remove("hidden-class");
-
-// });
-
-
-
-// 	    	setTimeout(function() {
-//     //your code/function here
-// }, time)
-
-// document.addEventListener('DOMContentLoaded', function() {
-// 	alert("Hello from JS!");
-// var clickas = document.querySelectorAll("header")[0];
-// // var y = document.body.scrollTop;
-// 	 window.addEventListener("scroll", function(){
-// 	 	var y = document.body.scrollTop;
-// 	    clickas.classList.add("vazinetojas");
-// 	    console.log(y);
-
-// 	});
-
-// });
